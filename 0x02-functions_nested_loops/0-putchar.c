3633905a181e29e277db05bd127d8985a5085473
simonkosisochukwu <simonkosisochukwu@gmail.com>
2022-09-19 04:44:03 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "_putchar.h"
-
 /**
- * main - program that prints _putchar.
+ * main - Entry point
  *
- * Return: int
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n = 0;
-	char str_hbt[9] = "_putchar";
+	char *sh = "_putchar";
 
-	while (n < 9)
+	while (*sh)
 	{
-		_putchar(str_hbt[n]);
-		n += 1;
+		_putchar(*sh);
+		sh++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
