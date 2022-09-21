5d0059fcb60320afd87367e8137d517346189420
simonkosisochukwu <simonkosisochukwu@gmail.com>
2022-09-21 15:29:19 -0700

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
-* void prototypes()
-* int prototypes()
-* char prototypes()
-*/
-
+int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -16,7 +8,5 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char _strcpy(char *dest, char *src);
-int atoi(char *str);
-
-#endif /*MAIN_H*/
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
