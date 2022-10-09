0832bddfb2aaae3ac1ac15449e3e7da61421b600
simonkosisochukwu <simonkosisochukwu@gmail.com>
2022-10-09 14:50:12 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,14 @@ char *create_array(unsigned int size, char c)
 	char *cr;
 	unsigned int i;
 	if (size == 0)
-		return (NULL);
+
+	  return (NULL);
 
 	cr = malloc(sizeof(c) * size);
 
 	if (cr == NULL)
-		return (NULL);
+
+	  return (NULL);
 
 	for (i = 0; i < size; i++)
 		cr[i] = c;
